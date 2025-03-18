f4143bdb6e3a6dc6dc220b9a805b656a0f131d8f
Ahmed Mamdoh <akremmamdoh@gmail.com>
2025-03-18 06:13:45 +0200

    Adding Timer Code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,15 @@
-document.getElementById("blogtab").addEventListener('mouseenter', function(){
-    document.getElementById("blogcont").style.display = "block"
-    if(document.getElementById("blogcont").style.display=="none"){
-        document.getElementById("blogcont").style.display = "block"
-    }
-    document.getElementById("blogcont").addEventListener('mouseleave', function(){
-        document.getElementById("blogcont").style.display = "none"
-    })
-})
+let time = 30 * 24 * 60 * 60;
+let countDown = document.querySelectorAll(".countDown");
+
+function updateCountdown() {
+  time--;
+  countDown[0].textContent = Math.floor(time / (24 * 60 * 60));
+  countDown[1].textContent = Math.floor((time % (24 * 60 * 60)) / (60 * 60));
+  countDown[2].textContent = Math.floor((time % (60 * 60)) / 60);
+  countDown[3].textContent = Math.floor(time % 60);
+  if (time <= 0) {
+    clearInterval(counter);
+  }
+}
+updateCountdown();
+let counter = setInterval(updateCountdown, 1000);
